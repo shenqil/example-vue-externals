d3874b3cde28ef96290708f7a3a638bea3c33815
fssqLove <248120694@qq.com>
2020-10-26 18:21:54 +0800

    删除无用文件

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,4 @@
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable @typescript-eslint/no-var-requires */
 // vue.config.js
-// const path = require('path');
-// const webpack = require('webpack');
-// const AddAssetHtmlWebpackPlugin = require('add-asset-html-webpack-plugin');
-
 if (process.env.NODE_ENV === 'production') {
   module.exports = {
     assetsDir: 'static',
@@ -15,23 +9,6 @@ if (process.env.NODE_ENV === 'production') {
         vuex: 'Vuex',
         'vue-router': 'VueRouter',
       },
-      // plugins: [
-      //   new webpack.DllReferencePlugin({
-      //     manifest: path.resolve(__dirname, 'dll/manifest.json'),
-      //   }),
-      //   new AddAssetHtmlWebpackPlugin([
-      //     {
-      //       filepath: path.resolve(__dirname, 'dll/vue.dll.js'),
-      //       publicPath: '../publicLib/',
-      //       outputPath: '../publicLib/',
-      //     },
-      //     {
-      //       filepath: path.resolve(__dirname, 'dll/elementUI.dll.js'),
-      //       publicPath: '../publicLib/',
-      //       outputPath: '../publicLib/',
-      //     },
-      //   ]),
-      // ],
     },
   };
 } else {
